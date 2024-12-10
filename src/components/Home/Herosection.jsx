4bad7c6fe670ce55ba766375e7d914b4b6045dcd
SasankaRoy <sasankaroy033@gmail.com>
2024-12-10 16:30:56 +0530

    small change

--- src/components/Home/Herosection.jsx
+++ src/components/Home/Herosection.jsx
@@ -1,4 +1,4 @@
-import { useScroll, useTransform, motion, useSpring } from "framer-motion";
+import { useScroll, useTransform, motion } from "framer-motion";
 import React, { useEffect, useRef, useState } from "react";
 
 export const Herosection = () => {
